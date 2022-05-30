ab031783231be3b15fe84612dfcf90823d255c81
Viren Thakur <viren.siyatechventures@gmail.com>
2022-05-30 12:33:58 +0530

    day 6 completed

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,8 +1,18 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_training/utils/MyRoutes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -12,55 +22,84 @@ class LoginPage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-            Image.asset(
-            "assets/images/login_image.png",
-            fit: BoxFit.cover,
-              height: 200,
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Text(
-            "Welcome",
-            style: TextStyle(
-                fontStyle: FontStyle.normal,
-                fontSize: 25,
-                fontWeight: FontWeight.bold),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Enter Username",
-                    labelText: "Username",
+              Image.asset(
+                "assets/images/login_image.png",
+                fit: BoxFit.cover,
+                height: 200,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Text(
+                "Welcome $name",
+                style: TextStyle(
+                    fontStyle: FontStyle.normal,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter Username",
+                        labelText: "Username",
+                      ),
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          hintText: "Enter password", labelText: "Password"),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              InkWell(
+                onTap:() async {
+                  setState(() {
+                    changeButton = true;
+                  });
+                  await Future.delayed(Duration(seconds: 1));
+                  Navigator.pushNamed(context, MyRoute.singupRoute);
+                },
+                child: AnimatedContainer(
+                  duration: Duration(seconds: 1),
+                  width: changeButton?50 : 150,
+                  height: 50,
+                  alignment: Alignment.center,
+                  child: changeButton?Icon(Icons.done,color: Colors.white,) : Text(
+                    "Login",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    // shape: changeButton ? BoxShape.circle:BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(changeButton?50:8)
                   ),
                 ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      hintText: "Enter password", labelText: "Password"),
-                )
-              ],
-            ),
+              )
+              /*  ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, MyRoute.singupRoute);
+                },
+                child: Text("Login"),
+                style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+              ),*/
+            ],
           ),
-          SizedBox(
-            height: 20.0,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, MyRoute.singupRoute);
-            },
-            child: Text("Login"),
-            style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-          ),
-
-      ],
-    ),
-        ),)
-    ,
+        ),
+      ),
     );
   }
-
 }
--- lib/practice_page.dart
+++ lib/practice_page.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_training/utils/MyRoutes.dart';
 
-class SignupPage extends StatelessWidget {
+class SignupPage extends StatefulWidget {
   const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  String name = "";
+  bool buttonChange = false;
 
   @override
   Widget build(BuildContext context) {
@@ -17,11 +25,23 @@ class SignupPage extends StatelessWidget {
             SizedBox(
               height: 40,
             ),
+            Text(
+              "Welcome $name",
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            ),
             Padding(
               padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
               child: TextFormField(
-                decoration:
-                    InputDecoration(hintText: "Enter Email", labelText: "Email"),
+                decoration: InputDecoration(
+                    hintText: "Enter Email", labelText: "Email"),
+                onChanged: (value) {
+                  setState(() {
+                    name = value;
+                  });
+                },
               ),
             ),
             Padding(
@@ -32,11 +52,42 @@ class SignupPage extends StatelessWidget {
                     hintText: "Enter Password", labelText: "Password"),
               ),
             ),
-            SizedBox(height: 40,),
-            ElevatedButton(onPressed: () {
-              Navigator.pushNamed(context, MyRoute.homeRoute);
-            }, child: Text("Signup"),
-            style: TextButton.styleFrom(minimumSize: Size(150, 45)),)
+            SizedBox(
+              height: 40,
+            ),
+            InkWell(
+              onTap:() async {
+                setState(() {
+                  buttonChange = true;
+                });
+
+                await Future.delayed(Duration(seconds: 1));
+                Navigator.pushNamed(context, MyRoute.homeRoute);
+              },
+              child: AnimatedContainer(
+                duration: Duration(seconds: 1),
+                width: buttonChange?50:150,
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(buttonChange?50:10)),
+                alignment: Alignment.center,
+                child: buttonChange? Icon(Icons.done,color: Colors.white,) :Text(
+                  "Login",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                ),
+              ),
+            )
+            /*  ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, MyRoute.homeRoute);
+              },
+              child: Text("Signup"),
+              style: TextButton.styleFrom(minimumSize: Size(150, 45)),
+            )*/
           ],
         ),
       ),
